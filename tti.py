16236968c3e74357c4677add103dba76bcae76a1
madhavansb <145147956+madhavansb@users.noreply.github.com>
2025-06-04 14:02:10 +0530

    Add files via upload

---
+++ tti.py
@@ -0,0 +1,21 @@
+!pip install diffusers transformers accelerate
+
+from diffusers import StableDiffusionPipeline
+import torch
+import matplotlib.pyplot as plt
+
+!pip show torch
+
+model1="dreamlike-art/dreamlike-diffusion-1.0"
+model2="stabilityai/stable-diffusion-xl-base-1.0"
+
+pipe=StableDiffusionPipeline.from_pretrained(model1,torch_dtype=torch.float16,use_safetensors=True)
+pipe.to("cuda")
+
+prompt="a single tree with plain lands"
+
+image=pipe(prompt).images[0]
+
+print("prompt : ", prompt)
+plt.imshow(image)
+plt.axis('off')
